b0ad78cdd49a35dd685994e89e1c0485eb2321cc
Harish-Fita-Trainer <Harish.Fita@gmail.com>
2024-03-21 09:09:11 +0530

    events changes

--- trainer/Js/index.js
+++ trainer/Js/index.js
@@ -4,7 +4,7 @@ function OnSave() {
     var pwd = document.getElementById("password").value;
     var cpwd = document.getElementById("confirmpassword").value;
     console.log("password", pwd, cpwd);
-    
+
     if (pwd === cpwd) {
         document.getElementById("message").setAttribute("class", "success")
         document.getElementById("message").innerText = "password matched";
@@ -16,10 +16,16 @@ function OnSave() {
     }
 }
 
+function OnDepartmentChange(event) {
+    console.log("OnDepartmentChange", event.target.value);
+}
+
 
 window.onload = function () {
     console.log("window onload")
-    document.getElementById("save_btn").addEventListener('click', OnSave);
+    //document.getElementById("save_btn").addEventListener('click', OnSave);
+    document.getElementById("department").addEventListener('change', OnDepartmentChange);
+
 }
 
 
