c37fac33ec1589fe91c48b1255d8ce76238d512a
VijayAnand2610 <vijayanandece2610@gmail.com>
2024-03-21 15:24:19 -1100

    style js changes

--- vijay/style.js
+++ vijay/style.js
@@ -14,9 +14,21 @@ function Onsubmit() {
         document.getElementById("message").innerText = "password not matched";
     }
 }
+function Onfirst_nameChange(event) {
+    console.log("Onfirst_nameChange", event.target.value);
+    if  (Name.length>="10") {
+        document.getElementById("name").setAttribute("class", "success")
+        document.getElementById("name").innerText = "Name found";
+    }
+    else  (  Name = null){
+        document.getElementById("name").setAttribute("class", "failure")
+        document.getElementById("name").innerText = "Name required";
+    }
 
+}
 
 window.onload = function () {
     console.log("window onload")
     document.getElementById("submit_btn").addEventListener('click', Onsubmit);
+    document.getElementById("first_name").addEventListener('change', Onfirst_nameChange);
 }
