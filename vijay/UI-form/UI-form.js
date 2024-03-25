25f45e7276b7858835303a50f8ed8d44c03b6780
VijayAnand2610 <vijayanandece2610@gmail.com>
2024-03-24 15:13:00 -1100

    updated

--- vijay/UI-form/UI-form.js
+++ vijay/UI-form/UI-form.js
@@ -1,83 +1,38 @@
 console.log("file loaded")
 
 function Onsubmit() {
-    var Name = document.getElementById("Name").value;
-    var Mobile No = document.getElementById("Mobile No").value;
+    var Name = document.getElementById("name").value;
+    var Mobile = document.getElementById("Mobile").value;
     var password = document.getElementById("password").value;
     var confirmpassword = document.getElementById("confirmpassword").value;
     console.log("password", password, confirmpassword);
     
 //--------Validation of Name------//  
-if( Name === "")
+if( Name == "")
 {
     document.getElementById("Name_error").setAttribute("class", "failure")
     document.getElementById("Name_error").innerText = "Enter The name";
 }
 else{
-    Name_error.innerHTML =""  
+    document.getElementById("Name_error").innerText = "";
 }
 //--------Validation of Mobile No------//  
-if( Mobile No === "")
+if( Mobile  == "")
 {
     document.getElementById("Mobile_No_error").setAttribute("class", "failure")
     document.getElementById("Mobile_No_error").innerText = "Enter The Mobile No";
 }
 else{
-    Mobile_No_error.innerHTML =""
+    document.getElementById("Mobile_No_error").innerText = "";
 }
-//--------Validation of Name------//  
-if( Name === "")
+//-----Validation of Email-id----//
+if( emailid == "")
 {
-    document.getElementById("Name_error").setAttribute("class", "failure")
-    document.getElementById("Name_error").innerText = "Enter The name";
+    document.getElementById("email_id_error").setAttribute("class", "failure")
+    document.getElementById("email_id_error").innerText = "Enter The  Email-id";
 }
 else{
-    Name_error.innerHTML =""  
-}
-//--------Validation of Name------//  
-if( Name === "")
-{
-    document.getElementById("Name_error").setAttribute("class", "failure")
-    document.getElementById("Name_error").innerText = "Enter The name";
-}
-else{
-    Name_error.innerHTML =""  
-}
-//--------Validation of Name------//  
-if( Name === "")
-{
-    document.getElementById("Name_error").setAttribute("class", "failure")
-    document.getElementById("Name_error").innerText = "Enter The name";
-}
-else{
-    Name_error.innerHTML =""  
-}
-//--------Validation of Name------//  
-if( Name === "")
-{
-    document.getElementById("Name_error").setAttribute("class", "failure")
-    document.getElementById("Name_error").innerText = "Enter The name";
-}
-else{
-    Name_error.innerHTML =""  
-}
-//--------Validation of Name------//  
-if( Name === "")
-{
-    document.getElementById("Name_error").setAttribute("class", "failure")
-    document.getElementById("Name_error").innerText = "Enter The name";
-}
-else{
-    Name_error.innerHTML =""  
-}
-//--------Validation of Name------//  
-if( Name === "")
-{
-    document.getElementById("Name_error").setAttribute("class", "failure")
-    document.getElementById("Name_error").innerText = "Enter The name";
-}
-else{
-    Name_error.innerHTML =""  
+    document.getElementById("email_id_error").innerText = "";
 }
 //------validation of password------//
 if (password === confirmpassword) {
@@ -93,5 +48,4 @@ else {
 window.onload = function () {
     console.log("window onload")
     document.getElementById("submit_btn").addEventListener('click', Onsubmit);
-    document.getElementById("first_name").addEventListener('change', Onfirst_nameChange);
 }
