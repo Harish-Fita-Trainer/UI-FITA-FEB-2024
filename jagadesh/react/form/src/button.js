58e033d8b816f59674d637e36ad7c80583a85601
Jagadeeshgajendran <jagadeshgajendran@gmail.com>
2024-04-18 07:43:17 +0530

    button

---
+++ jagadesh/react/form/src/button.js
@@ -0,0 +1,7 @@
+
+export default function Button(Props) {
+    const { ClassName, Text, OnClick } = Props;
+    return <div className={ClassName}>
+        <button onClick={OnClick}>{Text}</button>
+    </div>
+}
