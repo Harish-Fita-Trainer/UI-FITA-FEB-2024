f224f1167e3310807e0f26f8709f968aab8df7ea
Jagadeeshgajendran <jagadeshgajendran@gmail.com>
2024-04-21 23:03:26 +0530

    login authentication

---
+++ jagadesh/react/form/src/loginauthentication/input.js
@@ -0,0 +1,8 @@
+
+export default function Input({ type = "text", text, onChange, value }) {
+
+    return <div className="form-control">
+        <label>{text}</label>
+        <input type={type} onChange={onChange} value={value} />
+    </div>
+}
