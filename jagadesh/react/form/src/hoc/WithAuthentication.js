8c184e6add0bb5b2951b3599b5d0c1342665687e
Jagadeeshgajendran <jagadeshgajendran@gmail.com>
2024-04-23 06:24:13 +0530

    hoc

---
+++ jagadesh/react/form/src/hoc/WithAuthentication.js
@@ -0,0 +1,12 @@
+import React from "react";
+const userone=true;
+export default function Withathentication(Component)
+{
+    return function(){
+        if(!userone){
+            return<div>usernotAuthenticated</div>
+        }
+        return <Component/>;
+
+    };
+}//hoc
