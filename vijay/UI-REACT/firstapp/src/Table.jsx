731b40eef3494b9fc6f65e5eecb9f2745c6b2139
VijayAnand2610 <vijayanandece2610@gmail.com>
2024-04-23 12:40:00 -1100

    NEW tABLE

--- vijay/UI-REACT/firstapp/src/Table.jsx
+++ vijay/UI-REACT/firstapp/src/Table.jsx
@@ -2,5 +2,20 @@ import { useEffect, useState } from "react"
 import '.table.css';
 
 export default function AppuseEffect(){
-    const []
-}h
+    const [userslist, setuserslist] = useState([]);
+    const [id ,setid] =usestate('');
+    const [email,setemail] =usestate('');
+    const [name ,setname] =usestate('');
+    const [password ,setpassword] =usestate('');
+
+useEffect ( () => {
+    
+}
+
+
+
+
+
+
+
+}
